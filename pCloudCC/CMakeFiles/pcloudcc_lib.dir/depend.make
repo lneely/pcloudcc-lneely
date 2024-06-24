51db982ff6579518aaa018a5c6ce5fe423492fb0
Levi Neely <141506390+lneely@users.noreply.github.com>
2024-06-25 01:47:42 +0200

    removed cmake artifacts

--- pCloudCC/CMakeFiles/pcloudcc_lib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pcloudcc_lib.
-# This may be replaced when dependencies are built.
--- pCloudCC/CMakeFiles/pclsync.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pclsync"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pclsync.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
