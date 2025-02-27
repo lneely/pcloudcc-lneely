6cb36f8925a59474ef746058581846f9e0840d6f
Levi Neely <141506390+lneely@users.noreply.github.com>
2025-02-27 22:21:02 +0100

    prepare pcompat.h for decomposition

--- pclsync/pcompat.h
+++ pclsync/pcompat.h
@@ -92,17 +92,20 @@ typedef struct {
   psync_interface_t interfaces[];
 } psync_interface_list_t;
 
+// callback signatures
 typedef void (*psync_list_dir_callback)(void *, psync_pstat *);
 typedef void (*psync_list_dir_callback_fast)(void *, psync_pstat_fast *);
 typedef void (*psync_thread_start0)();
 typedef void (*psync_thread_start1)(void *);
 
+// constants
 #define INVALID_HANDLE_VALUE -1
 #define INVALID_SOCKET -1
 #define PSYNC_SOCKET_ERROR -1
 #define PSYNC_SOCKET_WOULDBLOCK -2
 #define SOCKET_ERROR -1
 
+// macros
 #define psync_32to64(hi, lo) ((((uint64_t)(hi)) << 32) + (lo))
 #define psync_bool_to_zero(x) (((int)(!!(x))) - 1)
 #define NTO_STR(s) TO_STR(s)
@@ -128,91 +131,107 @@ typedef void (*psync_thread_start1)(void *);
 #define psync_def_var_arr(name, type, size) type name[size]
 #define psync_stat_fast_isfolder(a) ((a)->isfolder)
 
-int intruncate(int fd);
-int intry_write_buffer(psync_socket *sock);
-int intry_write_buffer_thread(psync_socket *sock);
-void psync_anon_reset(void *ptr, size_t size);
+// System Initialization and Configuration
+void psync_sys_init();
+int psync_get_page_size();
+void psync_set_os_name(const char *osnm);
+void psync_set_software_name(const char *snm);
 const char *psync_appname();
-int psync_create_socket(int domain, int type, int protocol);
-char *psync_device_string();
-char *psync_deviceid();
-char *psync_deviceos();
-int psync_file_close(int fd);
-int psync_file_delete(const char *path);
-int psync_file_dup(int fd);
+int psync_stat_mode_ok(struct stat *buf, unsigned int bits) PSYNC_PURE;
+
+// Time and Sleep Functions
+time_t psync_time();
+uint64_t psync_millitime();
+void psync_milisleep(uint64_t millisec);
+void psync_milisleep_nosqlcheck(uint64_t millisec);
+
+// Memory Management
+void *psync_mmap_anon(size_t size);
+void *psync_mmap_anon_safe(size_t size);
+int psync_munmap_anon(void *ptr, size_t size);
+int psync_mlock(void *ptr, size_t size);
+int psync_munlock(void *ptr, size_t size);
+void psync_anon_reset(void *ptr, size_t size);
+void psync_get_random_seed(unsigned char *seed, const void *addent, size_t aelen, int fast);
+
+// Thread Management
+void psync_run_thread(const char *name, psync_thread_start0 run);
+void psync_run_thread1(const char *name, psync_thread_start1 run, void *ptr);
+
+// File Operations
 int psync_file_open(const char *path, int access, int flags);
-ssize_t psync_file_pread(int fd, void *buf, size_t count, uint64_t offset);
-int psync_file_preread(int fd, uint64_t offset, size_t count);
-ssize_t psync_file_pwrite(int fd, const void *buf, size_t count, uint64_t offset);
+int psync_file_close(int fd);
+int psync_file_dup(int fd);
 ssize_t psync_file_read(int fd, void *buf, size_t count);
-int psync_file_readahead(int fd, uint64_t offset, size_t count);
-int psync_file_rename(const char *oldpath, const char *newpath);
-int psync_file_rename_overwrite(const char *oldpath, const char *newpath);
-int psync_file_schedulesync(int fd);
+ssize_t psync_file_write(int fd, const void *buf, size_t count);
+ssize_t psync_file_pread(int fd, void *buf, size_t count, uint64_t offset);
+ssize_t psync_file_pwrite(int fd, const void *buf, size_t count, uint64_t offset);
 int64_t psync_file_seek(int fd, uint64_t offset, int whence);
-int psync_file_set_creation(int fd, time_t ctime);
 int64_t psync_file_size(int fd);
 int psync_file_sync(int fd);
-ssize_t psync_file_write(int fd, const void *buf, size_t count);
+int psync_file_schedulesync(int fd);
+int psync_file_readahead(int fd, uint64_t offset, size_t count);
+int psync_file_preread(int fd, uint64_t offset, size_t count);
+int psync_file_delete(const char *path);
+int psync_file_rename(const char *oldpath, const char *newpath);
+int psync_file_rename_overwrite(const char *oldpath, const char *newpath);
+int psync_file_set_creation(int fd, time_t ctime);
+int intruncate(int fd);
 int psync_folder_sync(const char *path);
-char *psync_get_default_database_path();
-int64_t psync_get_free_space_by_path(const char *path);
+int psync_run_update_file(const char *path);
+int psync_set_crtime_mtime(const char *path, time_t crtime, time_t mtime);
+int psync_set_crtime_mtime_by_fd(int fd, const char *path, time_t crtime, time_t mtime);
+int psync_invalidate_os_cache(const char *path);
+int psync_invalidate_os_cache_needed();
+
+// Directory and Path Operations
+int psync_list_dir(const char *path, psync_list_dir_callback callback, void *ptr);
+int psync_list_dir_fast(const char *path, psync_list_dir_callback_fast callback, void *ptr);
 char *psync_get_home_dir();
-int psync_get_page_size();
 char *psync_get_pcloud_path();
 char *psync_get_private_dir(char *name);
 char *psync_get_private_tmp_dir();
-void psync_get_random_seed(unsigned char *seed, const void *addent, size_t aelen, int fast);
-int psync_invalidate_os_cache(const char *path);
-int psync_invalidate_os_cache_needed();
-int psync_list_dir(const char *path, psync_list_dir_callback callback, void *ptr);
-int psync_list_dir_fast(const char *path, psync_list_dir_callback_fast callback, void *ptr);
-psync_interface_list_t *psync_list_ip_adapters();
-void psync_milisleep(uint64_t millisec);
-void psync_milisleep_nosqlcheck(uint64_t millisec);
-uint64_t psync_millitime();
-int psync_mlock(void *ptr, size_t size);
-void *psync_mmap_anon(size_t size);
-void *psync_mmap_anon_safe(size_t size);
-int psync_munlock(void *ptr, size_t size);
-int psync_munmap_anon(void *ptr, size_t size);
-void psync_rebuild_icons();
-void psync_run_thread(const char *name, psync_thread_start0 run);
-void psync_run_thread1(const char *name, psync_thread_start1 run, void *ptr);
-int psync_run_update_file(const char *path);
-int psync_select_in(int *sockets, int cnt, int64_t timeoutmillisec);
-int psync_set_crtime_mtime(const char *path, time_t crtime, time_t mtime);
-int psync_set_crtime_mtime_by_fd(int fd, const char *path, time_t crtime, time_t mtime);
-void psync_set_os_name(const char *osnm);
-void psync_set_software_name(const char *snm);
-void psync_socket_clear_write_buffered(psync_socket *sock);
-void psync_socket_clear_write_buffered_thread(psync_socket *sock);
+char *psync_get_default_database_path();
+int64_t psync_get_free_space_by_path(const char *path);
+
+// Socket and Network Operations
+int psync_create_socket(int domain, int type, int protocol);
+psync_socket *psync_socket_connect(const char *host, int unsigned port, int ssl);
 void psync_socket_close(psync_socket *sock);
 void psync_socket_close_bad(psync_socket *sock);
-psync_socket *psync_socket_connect(const char *host, int unsigned port, int ssl);
-int psync_socket_is_broken(int sock);
+int psync_socket_read(psync_socket *sock, void *buff, int num);
+int psync_socket_write(psync_socket *sock, const void *buff, int num);
+int psync_socket_readall(psync_socket *sock, void *buff, int num);
+int psync_socket_writeall(psync_socket *sock, const void *buff, int num);
+int psync_socket_read_noblock(psync_socket *sock, void *buff, int num);
+int psync_socket_read_thread(psync_socket *sock, void *buff, int num);
+int psync_socket_readall_thread(psync_socket *sock, void *buff, int num);
+int psync_socket_writeall_thread(psync_socket *sock, const void *buff, int num);
 int psync_socket_isssl(psync_socket *sock) PSYNC_PURE;
+int psync_socket_is_broken(int sock);
+int psync_socket_readable(psync_socket *sock);
+int psync_socket_writable(psync_socket *sock);
 int psync_socket_pendingdata(psync_socket *sock);
 int psync_socket_pendingdata_buf(psync_socket *sock);
 int psync_socket_pendingdata_buf_thread(psync_socket *sock);
-int psync_socket_read(psync_socket *sock, void *buff, int num);
-int psync_socket_read_noblock(psync_socket *sock, void *buff, int num);
-int psync_socket_read_thread(psync_socket *sock, void *buff, int num);
-int psync_socket_readable(psync_socket *sock);
-int psync_socket_readall(psync_socket *sock, void *buff, int num);
-int psync_socket_readall_thread(psync_socket *sock, void *buff, int num);
 int psync_socket_set_recvbuf(psync_socket *sock, int bufsize);
 int psync_socket_set_sendbuf(psync_socket *sock, int bufsize);
 void psync_socket_set_write_buffered(psync_socket *sock);
+void psync_socket_clear_write_buffered(psync_socket *sock);
 void psync_socket_set_write_buffered_thread(psync_socket *sock);
-int psync_socket_writable(psync_socket *sock);
-int psync_socket_write(psync_socket *sock, const void *buff, int num);
-int psync_socket_writeall(psync_socket *sock, const void *buff, int num);
-int psync_socket_writeall_thread(psync_socket *sock, const void *buff, int num);
-int psync_stat_mode_ok(struct stat *buf, unsigned int bits) PSYNC_PURE;
-void psync_sys_init();
-time_t psync_time();
+void psync_socket_clear_write_buffered_thread(psync_socket *sock);
+int intry_write_buffer(psync_socket *sock);
+int intry_write_buffer_thread(psync_socket *sock);
 int psync_wait_socket_read_timeout(int sock);
 int psync_wait_socket_write_timeout(int sock);
+int psync_select_in(int *sockets, int cnt, int64_t timeoutmillisec);
+psync_interface_list_t *psync_list_ip_adapters();
+
+// Device Information
+char *psync_device_string();
+char *psync_deviceid();
+char *psync_deviceos();
+void psync_rebuild_icons();
+
 
 #endif
