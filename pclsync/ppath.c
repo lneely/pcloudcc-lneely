9a9fdfc3bfe5a2183ba05772b32801b1d91bd054
Levi Neely <141506390+lneely@users.noreply.github.com>
2025-02-28 18:16:22 +0100

    format

--- pclsync/ppath.c
+++ pclsync/ppath.c
@@ -3,29 +3,29 @@
 #include <stddef.h>
 #include <sys/statvfs.h>
 
-#include "ppath.h"
 #include "pcompiler.h"
 #include "plibs.h"
+#include "ppath.h"
 #include "psettings.h"
 
 char *ppath_default_db() {
   char *pcdir, *dbp, *home, *oldp;
   struct stat st;
-  
+
   pcdir = ppath_pcloud();
   if (!pcdir) {
     return NULL;
   }
-    
+
   dbp = psync_strcat(pcdir, "/", PSYNC_DEFAULT_DB_NAME, NULL);
   psync_free(pcdir);
-  
+
   if (stat(dbp, &st)) {
     // Inline the old database path function here
     if ((home = ppath_home())) {
       oldp = psync_strcat(home, "/", PSYNC_DEFAULT_POSIX_DBNAME, NULL);
       psync_free(home);
-      
+
       if (oldp) {
         if (!stat(oldp, &st)) {
           if (psync_sql_reopen(oldp)) {
@@ -39,7 +39,7 @@ char *ppath_default_db() {
       }
     }
   }
-  
+
   return dbp;
 }
 
@@ -75,37 +75,37 @@ int ppath_ls(const char *path, ppath_ls_cb callback, void *ptr) {
   char *cpath;
   size_t pl;
   struct dirent *de;
-  
+
   dh = opendir(path);
   if (unlikely(!dh)) {
     debug(D_WARNING, "could not open directory %s", path);
     psync_error = PERROR_LOCAL_FOLDER_NOT_FOUND;
     return -1;
   }
-  
+
   pl = strlen(path);
   cpath = (char *)psync_malloc(pl + NAME_MAX + 2);
   memcpy(cpath, path, pl);
   if (!pl || cpath[pl - 1] != '/')
     cpath[pl++] = '/';
-  
+
   pst.path = cpath;
-  
+
   while ((de = readdir(dh))) {
     // Skip . and .. entries
-    if (de->d_name[0] == '.' && 
+    if (de->d_name[0] == '.' &&
         (de->d_name[1] == 0 || (de->d_name[1] == '.' && de->d_name[2] == 0)))
       continue;
-      
+
     psync_strlcpy(cpath + pl, de->d_name, NAME_MAX + 1);
-    
+
     if (likely_log(!lstat(cpath, &pst.stat)) &&
         (S_ISREG(pst.stat.st_mode) || S_ISDIR(pst.stat.st_mode))) {
       pst.name = de->d_name;
       callback(ptr, &pst);
     }
   }
-  
+
   psync_free(cpath);
   closedir(dh);
   return 0;
@@ -118,36 +118,34 @@ int ppath_ls_fast(const char *path, ppath_ls_fast_cb callback, void *ptr) {
   char *cpath;
   size_t pl;
   struct dirent *de;
-  
+
   dh = opendir(path);
   if (unlikely_log(!dh)) {
     psync_error = PERROR_LOCAL_FOLDER_NOT_FOUND;
     return -1;
   }
-  
+
   pl = strlen(path);
   cpath = (char *)psync_malloc(pl + NAME_MAX + 2);
   memcpy(cpath, path, pl);
   if (!pl || cpath[pl - 1] != '/')
     cpath[pl++] = '/';
-  
+
   while ((de = readdir(dh))) {
     // Skip . and .. entries
-    if (de->d_name[0] == '.' && 
+    if (de->d_name[0] == '.' &&
         (de->d_name[1] == 0 || (de->d_name[1] == '.' && de->d_name[2] == 0)))
       continue;
-      
+
     pst.name = de->d_name;
-    
+
     if (de->d_type == DT_DIR) {
       pst.isfolder = 1;
       callback(ptr, &pst);
-    } 
-    else if (de->d_type == DT_REG) {
+    } else if (de->d_type == DT_REG) {
       pst.isfolder = 0;
       callback(ptr, &pst);
-    }
-    else if (de->d_type == DT_UNKNOWN) {
+    } else if (de->d_type == DT_UNKNOWN) {
       // Fall back to lstat for unknown file types
       psync_strlcpy(cpath + pl, de->d_name, NAME_MAX + 1);
       if (!unlikely_log(lstat(cpath, &st))) {
@@ -157,7 +155,7 @@ int ppath_ls_fast(const char *path, ppath_ls_fast_cb callback, void *ptr) {
     }
     // Ignore other file types
   }
-  
+
   psync_free(cpath);
   closedir(dh);
   return 0;
@@ -166,7 +164,7 @@ int ppath_ls_fast(const char *path, ppath_ls_fast_cb callback, void *ptr) {
 char *ppath_pcloud() {
   char *homedir, *path;
   struct stat st;
-  
+
   if (!(homedir = ppath_home())) {
     return NULL;
   }
@@ -174,13 +172,14 @@ char *ppath_pcloud() {
   path = psync_strcat(homedir, "/", PSYNC_DEFAULT_POSIX_DIR, NULL);
   psync_free(homedir);
   if (unlikely_log(!path)) {
-    return NULL; 
+    return NULL;
   }
 
-  if (stat(path, &st) && unlikely_log(mkdir(path, PSYNC_DEFAULT_POSIX_FOLDER_MODE))) {
+  if (stat(path, &st) &&
+      unlikely_log(mkdir(path, PSYNC_DEFAULT_POSIX_FOLDER_MODE))) {
     psync_free(path);
     return NULL;
-  } 
+  }
   return path;
 }
 
@@ -199,6 +198,4 @@ char *ppath_private(char *name) {
   return rpath;
 }
 
-char *ppath_private_tmp() {
-  return ppath_private(PSYNC_DEFAULT_TMP_DIR);
-}
+char *ppath_private_tmp() { return ppath_private(PSYNC_DEFAULT_TMP_DIR); }
