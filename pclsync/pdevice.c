7f24fabc540aacaf603af1a1b7598dd2dad1e28b
Levi Neely <lkn@darkstar.example.net>
2026-03-08 19:55:21 +0100

    Fix all remaining stray free() calls - migrate to pmem_free()

--- pclsync/pdevice.c
+++ pclsync/pdevice.c
@@ -61,7 +61,7 @@ char *pdevice_id() {
         path =
             putil_strcat("/sys/class/power_supply/", de->d_name, "/type", NULL);
         fd = open(path, O_RDONLY);
-        free(path);
+        pmem_free(PMEM_SUBSYS_OTHER, path);
         if (fd == -1)
           continue;
         if (read(fd, buf, 7) == 7 && !memcmp(buf, "Battery", 7)) {
@@ -81,7 +81,7 @@ char *pdevice_id() {
 char *pdevice_name() {
   char *osname = pdevice_get_os();
   char *ret = putil_strcat(osname, ", ", psync_software_name, NULL);
-  free(osname);
+  pmem_free(PMEM_SUBSYS_OTHER, osname);
   return ret;
 }
 
