fc44baab8b8536df69c953a34340b64ec4a7ea5f
Levi Neely <lkn@darkstar.example.net>
2026-03-08 15:45:26 +0100

    Implement memory accounting infrastructure

--- pclsync/pmem.h
+++ pclsync/pmem.h
@@ -4,6 +4,15 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef enum {
+  PMEM_SUBSYS_API,
+  PMEM_SUBSYS_CACHE,
+  PMEM_SUBSYS_UPLOAD,
+  PMEM_SUBSYS_DOWNLOAD,
+  PMEM_SUBSYS_SYNC,
+  PMEM_SUBSYS_OTHER
+} pmem_subsystem_t;
+
 void *pmem_mmap(size_t size);
 void *pmem_mmap_safe(size_t size);
 int pmem_munmap(void *ptr, size_t size);
@@ -11,5 +20,9 @@ int pmem_mlock(void *ptr, size_t size);
 int pmem_munlock(void *ptr, size_t size);
 void pmem_reset(void *ptr, size_t size);
 void *pmem_calloc_safe(size_t nmemb, size_t size);
+void *pmem_malloc(pmem_subsystem_t subsystem, size_t size);
+void pmem_free(pmem_subsystem_t subsystem, void *ptr);
+void *pmem_realloc(pmem_subsystem_t subsystem, void *ptr, size_t size);
+size_t pmem_get_stats(pmem_subsystem_t subsystem);
 
 #endif
